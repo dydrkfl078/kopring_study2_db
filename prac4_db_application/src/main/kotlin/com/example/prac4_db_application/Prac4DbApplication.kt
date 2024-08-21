4bb499dc0481a46149c86b65b29292be6c6940c6
june <dydrkfl078@naver.com>
2024-08-21 11:04:05 +0900

    :white_check_mark: Embedded Test [ test-resource 에 schema.sql 추가 ] - 별도로 DB 서버를 띄우지 않아도 메모리로 테스트가 가능하다

--- prac4_db_application/src/main/kotlin/com/example/prac4_db_application/Prac4DbApplication.kt
+++ prac4_db_application/src/main/kotlin/com/example/prac4_db_application/Prac4DbApplication.kt
@@ -3,16 +3,20 @@ package com.example.prac4_db_application
 import com.example.prac4_db_application.config.JdbcConfig
 import com.example.prac4_db_application.config.MemoryConfig
 import com.example.prac4_db_application.repository.ItemRepository
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
+import org.springframework.jdbc.datasource.DriverManagerDataSource
 import org.springframework.stereotype.Component
+import javax.sql.DataSource
 
 @Import(JdbcConfig::class)
 @SpringBootApplication
 class Prac4DbApplication {
+
 
 }
 
@@ -21,8 +25,6 @@ fun main(args: Array<String>) {
 
 }
 
-@Bean
-fun testDataInit(itemRepository: ItemRepository): TestDataInit {
-    return TestDataInit(itemRepository)
-}
+
+
 
