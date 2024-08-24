e490cae2d8bf0910a19bdd4ee8d822fbc6eb3365
june <dydrkfl078@naver.com>
2024-08-25 01:47:19 +0900

    :tada: Spring Transaction 학습 시작

---
+++ prac5_spring_transaction/src/main/kotlin/kopring/prac5_spring_transaction/Prac5SpringTransactionApplication.kt
@@ -0,0 +1,11 @@
+package kopring.prac5_spring_transaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Prac5SpringTransactionApplication
+
+fun main(args: Array<String>) {
+    runApplication<Prac5SpringTransactionApplication>(*args)
+}
---
+++ prac5_spring_transaction/src/test/kotlin/kopring/prac5_spring_transaction/Prac5SpringTransactionApplicationTests.kt
@@ -0,0 +1,13 @@
+package kopring.prac5_spring_transaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Prac5SpringTransactionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
